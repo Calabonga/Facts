f14e0a0376ce81179fff0ada7689c5b15bfe426d
Calabonga <dev@calabonga.net>
2021-05-31 09:24:37 +0700

    Добавлена информация о бранче и версии

---
+++ src/Calabonga.Facts/Calabonga.Facts.Web/Infrastructure/Services/IVersionInfoService.cs
@@ -0,0 +1,11 @@
+﻿namespace Calabonga.Facts.Web.Infrastructure.Services
+{
+    public interface IVersionInfoService
+    {
+        string Version { get; }
+
+        string Branch { get; }
+
+        string Commit { get; }
+    }
+}
